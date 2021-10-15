f0f37be99360749ace1043c76eceb15621a4ce42
ABanbu <89296326+ABanbu@users.noreply.github.com>
2021-10-15 14:20:29 +0800

    changes daily wallpaper

--- src/main/java/com/Wallpaper.java
+++ src/main/java/com/Wallpaper.java
@@ -6,8 +6,8 @@ public class Wallpaper {
 
     public static void main(String[] args) throws IOException {
         System.out.println("========开始下载今日Bing壁纸=======");
-        new ImageUtils().imagesDownloader();
-        //new ImageUtils().imageDownloader();
+        //new ImageUtils().imagesDownloader();
+        new ImageUtils().imageDownloader();
         System.out.println("===========下载壁纸结束==========");
     }
 }
